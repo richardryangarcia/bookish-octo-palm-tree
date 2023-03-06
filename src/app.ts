2f4f6cb9b74cc4507d036e26c10bc91669c93943
Richard Ryan <richardryangarcia@gmail.com>
2023-03-06 15:30:34 -0600

    init commit

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import { createPublicClient, http } from "viem";
+import { mainnet } from "viem/chains";
+
+const app = express();
+const port = 3001;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  return console.log(`Express is listening at http://localhost:${port}`);
+});
+
+const main = async () => {
+  console.log("main started");
+};
+
+main();
